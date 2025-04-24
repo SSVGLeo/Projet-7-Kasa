0d4b9d3820fb3f872d1ba55e114e687b66b1609b
SSVGLeo <troupleo@gmail.com>
2025-04-24 09:25:13 +0200

    Ajout de la fonction gitpages avec react

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./utils/styles/styles.css";
 
 // Importation of global elements
